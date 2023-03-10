5ad91217616cb80a7571d8b338c426adeb52cbd2
Erfan <erfannavab@gmail.com>
2023-03-10 21:33:53 +0330

    Initial commit

---
+++ src/test/java/nvb/spring/cruddemo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nvb.spring.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
